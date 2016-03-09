4177d3b2a46f20a899e3ab4bd2489392a578f3ec
ronnieholm <mail@bugfree.dk>
2016-03-09 11:56:46 +0100

    Updated

--- src/Bugfree.SharePoint.Analyzer.Importer/AssemblyInfo.fs
+++ src/Bugfree.SharePoint.Analyzer.Importer/AssemblyInfo.fs
@@ -5,7 +5,7 @@ open System.Reflection
 [<assembly: AssemblyTitle("Bugfree.SharePoint.Analyzer.Importer")>]
 [<assembly: AssemblyDescription("")>]
 [<assembly: AssemblyProduct("Bugfree.SharePoint.Analyzer.Importer")>]
-[<assembly: AssemblyCopyright("Copyright (c) 2014-2015 Ronnie Holm <mail@bugfree.dk>")>]
+[<assembly: AssemblyCopyright("Copyright (c) 2016 Ronnie Holm <mail@bugfree.dk>")>]
 [<assembly: AssemblyVersion("1.0.0.0")>]
 [<assembly: AssemblyFileVersion("1.0.0.0")>]
 
