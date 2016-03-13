e414aa458bcf818eb6f9db7f4d83b1e55eef1649
ronnieholm <mail@bugfree.dk>
2016-03-13 21:26:54 +0100

    Updated

--- build.ps1
+++ build.ps1
@@ -4,8 +4,8 @@ $import_base_path = "$($base_path)\src\Bugfree.SharePoint.Analyzer.Importer"
 $new_db_name = "WebApplications"
 $mdf_file_path = "$($import_base_path)\$($new_db_name).mdf"
 $log_file_path = "$($import_base_path)\$($new_db_name).ldf"
-$detachdb_sql_path = "$($import_base_path)\detachdb.sql"
-$createdb_sql_path = "$($import_base_path)\createdb.sql"
+$detachdb_path = "$($import_base_path)\detachdb.sql"
+$createdb_path = "$($import_base_path)\createdb.sql"
 
 $detach_db_sql = @"
     USE master;
@@ -14,7 +14,7 @@ $detach_db_sql = @"
     GO
 "@
 
-$detach_db_sql | Out-File $detachdb_sql_path
+$detach_db_sql | Out-File $detachdb_path
 
 if (Test-Path $mdf_file_path) {
     Remove-Item $mdf_file_path
@@ -47,9 +47,9 @@ $create_db_sql = @"
 
 $schema = Get-Content "$($import_base_path)\schema.sql"
 $create_db_sql = $create_db_sql + $schema
-$create_db_sql | Out-File $createdb_sql_path
-&$sqlcmd -S "(localdb)\v11.0" -i $createdb_sql_path
-&$sqlcmd -S "(localdb)\v11.0" -i $detachdb_sql_path
+$create_db_sql | Out-File $createdb_path
+&$sqlcmd -S "(localdb)\v11.0" -i $createdb_path
+&$sqlcmd -S "(localdb)\v11.0" -i $detachdb_path
 
 $msbuild = "C:\Program Files (x86)\MSBuild\14.0\bin\amd64\msbuild.exe"
 &$msbuild "$($base_path)\src\Bugfree.SharePoint.Analyzer.sln"
